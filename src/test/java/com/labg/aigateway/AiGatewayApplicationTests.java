1feba9ce8e86f28a123a5b955e26cdc83708f178
이가은 <kaeun0320@labgenomics.com>
2025-10-28 13:49:39 +0900

    init spring boot project

---
+++ src/test/java/com/labg/aigateway/AiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.labg.aigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
